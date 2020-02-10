9bcf56d2a44b6f38cb2fef075cae117d8c0903b8
Matthew Pinder <b6019406@my.shu.ac.uk>
2020-02-10 14:26:52 +0000

    Started detail overview

---
+++ client/src/views/DetailOverview.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h3>{{ $route.params.id }}</h3>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-outline-secondary">Make an Appointment</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
