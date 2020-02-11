e0ef21f76281cb76fd952b98b49d96c17dfd8a0a
Sam D Harris <samdharris@outlook.com>
2020-02-11 08:35:24 +0000

    Modify users table migration adding additional fields

--- server/src/database/migrations/20200120165048_create_users_table.js
+++ server/src/database/migrations/20200120165048_create_users_table.js
@@ -1,7 +1,13 @@
+const { ACCOUNT_TYPES } = require('../../constants');
 exports.up = function(knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id');
     table.string('name', 255).notNullable();
+    table.string('phone_number', 11);
+    table.string('email_address', 255);
+    table
+      .enum('account_type', Object.values(ACCOUNT_TYPES))
+      .default(ACCOUNT_TYPES.ACCOUNT_PATIENT);
     table.string('password', 255).notNullable();
     table.timestamps();
   });
