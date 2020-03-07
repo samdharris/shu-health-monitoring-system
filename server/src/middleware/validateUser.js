fadc508b2f575bcaaaa8c70677fdeed47dcfa3f6
Sam D Harris <samdharris@outlook.com>
2020-03-07 16:20:58 +0000

    Add validate user middleware

---
+++ server/src/middleware/validateUser.js
@@ -0,0 +1,12 @@
+const authenticatorService = require('../services/authenticatorService');
+const httpCodes = require('http-status-codes');
+
+module.exports = (req, res, next) => {
+    try {
+        const {user} = await authenticatorService.authenticateWithToken(req.header('Authorization'))
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(httpCodes.UNAUTHORIZED).send();
+    }
+}
