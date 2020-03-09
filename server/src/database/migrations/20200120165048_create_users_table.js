34c5f9a7fddf4fcb9910ce9bbe0b5af379e57508
Sam D Harris <samdharris@outlook.com>
2020-03-09 13:18:32 +0000

    Remove columns

--- server/src/database/migrations/20200120165048_create_users_table.js
+++ server/src/database/migrations/20200120165048_create_users_table.js
@@ -1,31 +1,18 @@
-const { ACCOUNT_TYPES } = require("../../constants");
+const { ACCOUNT_TYPES } = require('../../constants');
 exports.up = function(knex) {
-  return knex.schema.createTable("users", table => {
-    table.increments("id");
-    table.string("name", 255).notNullable();
-    table.string("phone_number", 11);
-    table.string("email_address", 255);
+  return knex.schema.createTable('users', table => {
+    table.increments('id');
+    table.string('name', 255).notNullable();
+    table.string('phone_number', 11);
+    table.string('email_address', 255);
     table
-      .enum("account_type", Object.values(ACCOUNT_TYPES))
+      .enum('account_type', Object.values(ACCOUNT_TYPES))
       .default(ACCOUNT_TYPES.ACCOUNT_PATIENT);
-
-    table
-      .bigInteger("gp_id")
-      .unsigned()
-      .index()
-      .references("id")
-      .inTable("gp");
-    table
-      .bigInteger("address_id")
-      .unsigned()
-      .index()
-      .references("id")
-      .inTable("address");
-    table.string("password", 255).notNullable();
+    table.string('password', 255).notNullable();
     table.timestamps(true, true);
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema.dropTableIfExists('users');
 };
