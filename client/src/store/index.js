8ea0ef6bdbfe13e5ecbe5c5b2e7a8e605380fde8
Sam D Harris <samdharris@outlook.com>
2020-03-10 17:32:32 +0000

    Wired up frontend for making appointments

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -22,8 +22,17 @@ export default new Vuex.Store({
   actions: {
     async makeAppointment({ commit }, appointment) {
       try {
-        await axios.post('/api/appointments', appointment);
-        router.push('/');
+        await axios.post(
+          'http://localhost:3001/api/appointments',
+          appointment,
+          {
+            headers: {
+              Authorization: `bearer ${localStorage.getItem('token')}`
+            }
+          }
+        );
+        // go back
+        router.go(-1);
       } catch (error) {
         commit('showError', error.response.data.message);
       }
