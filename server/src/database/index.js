072d4934bbde448df1ae5be82cf13560650fc471
Sam D Harris <samdharris@outlook.com>
2020-03-14 15:05:31 +0000

    Fix db port issue

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -12,7 +12,7 @@ exports.start = () => {
       client: 'mysql',
       connection: {
         host: process.env.DB_HOST,
-        port: process.env.port || 3306,
+        port: process.env.DB_PORT || 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DATABASE
