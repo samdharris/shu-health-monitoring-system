18a2f7da691c2c8207660b870c329411ff00ccc8
Sam D Harris <samdharris@outlook.com>
2020-03-16 18:15:07 +0000

    Document tests

--- server/test/appointment.test.js
+++ server/test/appointment.test.js
@@ -6,6 +6,10 @@ const faker = require('faker');
 
 const path = require('path');
 
+/**
+ * Runs before any test block is executed. Used to connect to the database and run migrations.
+ * @see https://jestjs.io/docs/en/api#beforeallfn-timeout
+ */
 beforeAll(() => {
   database.start();
   /**
@@ -16,8 +20,15 @@ beforeAll(() => {
   });
 });
 
+/**
+ * Test block to test making an appointment
+ */
 describe('POST - /api/appointments', () => {
   let data = {};
+  /**
+   * Runs before any test in this test block is run. Used to seed a user and to get authentication.
+   * @see https://jestjs.io/docs/en/api#beforeallfn-timeout
+   */
   beforeAll(() => {
     // Login the user
     return userSeeder
@@ -39,6 +50,7 @@ describe('POST - /api/appointments', () => {
   });
 
   it('should create an appointment', async () => {
+    // Arrange
     const date = new Date();
     date.setMinutes(15, 0, 0);
     const appointment = {
@@ -46,11 +58,13 @@ describe('POST - /api/appointments', () => {
       reason: faker.lorem.sentence()
     };
 
+    // Act
     const response = await supertest
       .post('/api/appointments')
       .set('Authorization', `bearer ${data.token}`)
       .send(appointment);
 
+    // Assert
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('appointment');
     expect(response.body.appointment).toEqual(
