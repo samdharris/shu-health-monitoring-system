9330f514c33d89376403ae910b1d4f072ca211ec
Sam D Harris <samdharris@outlook.com>
2020-03-16 18:17:58 +0000

    document validators

--- server/src/validation/dataValidator.js
+++ server/src/validation/dataValidator.js
@@ -1,6 +1,12 @@
 const Joi = require('@hapi/joi');
 
+/**
+ * Data Validator
+ */
 const schema = Joi.object({
+  /**
+   * The provided value must be a number as well as positive. It is a required field
+   */
   value: Joi.number()
     .positive()
     .required()
--- server/src/validation/login.js
+++ server/src/validation/login.js
@@ -1,8 +1,17 @@
 const Joi = require('@hapi/joi');
 
+/**
+ * Login Validation
+ */
 module.exports = Joi.object({
+  /**
+   * An email address must be a string and is a required field
+   */
   email: Joi.string()
     .email()
     .required(),
+  /**
+   * A Password must be a string and is a required field
+   */
   password: Joi.string().required()
 });
