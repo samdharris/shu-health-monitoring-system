903dcebe939b946cf7cb4ebec8c763249b0985ed
Sam D Harris <samdharris@outlook.com>
2020-03-16 20:01:16 +0000

    Document repositories

--- server/src/database/repositories/appointmentRepository.js
+++ server/src/database/repositories/appointmentRepository.js
@@ -1,3 +1,10 @@
 const databaseService = require('../../services/databaseService');
+
+/**
+ * Sends a booking the to database for storage
+ *
+ * @param {Object} booking
+ * @returns {Promise<Object>}
+ */
 exports.bookAppointment = async booking =>
   await databaseService.addData('appointments', booking);
--- server/src/database/repositories/integrationsRepository.js
+++ server/src/database/repositories/integrationsRepository.js
@@ -1,4 +1,13 @@
 const databaseService = require('../../services/databaseService');
+
+/**
+ * Updates the given set of integration data
+ *
+ * @param {Number} userIntegrationId
+ * @param {Object} data
+ *
+ * @returns {Promise<Object>}
+ */
 exports.updateData = async (userIntegrationId, data) => {
   return await databaseService.updateData(
     'integrations_data',
@@ -8,12 +17,26 @@ exports.updateData = async (userIntegrationId, data) => {
   );
 };
 
+/**
+ * Creates a new reading
+ *
+ * @param {Number} integrationId
+ * @param {Number} value
+ *
+ * @returns {Promise<Object>}
+ */
 exports.addData = async (integrationId, value) =>
   await databaseService.addData('integrations_data', {
     user_integration_id: integrationId,
     value
   });
 
+/**
+ * Gets all integrations for a given user
+ *
+ * @param {Number} userId
+ * @returns {Promise<Array<Object>>}
+ */
 exports.getUserIntegrations = async userId => {
   return await databaseService.getData('user_integrations', builder => {
     return builder
@@ -29,6 +52,12 @@ exports.getUserIntegrations = async userId => {
   });
 };
 
+/**
+ * Returns all data for the given integration
+ *
+ * @param {Number} userIntegrationId
+ * @returns {Promise<Array<Object>>}
+ */
 exports.getDataForIntegration = async userIntegrationId => {
   return await databaseService.getData('integrations_data', builder => {
     return builder
