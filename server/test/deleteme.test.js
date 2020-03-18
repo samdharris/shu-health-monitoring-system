4e1219cc040f82d64e9089356941b1181f59f44f
Sam D Harris <samdharris@outlook.com>
2020-03-18 11:37:26 +0000

    Add a test to demo ci

---
+++ server/test/deleteme.test.js
@@ -0,0 +1,12 @@
+describe('This is a demo to prove Sam can walk the walk.', () => {
+  it('should add 5+5 to make 10', () => {
+    // Arrange
+    const expected = 10;
+
+    // Act
+    const actual = 5 + 6;
+
+    // Expect
+    expect(actual).toBe(expected);
+  });
+});
